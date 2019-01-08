28878d7d5e2260cd37f0ed899add196b710dcc87
José Abarca Saavedra <jose@smart.rancagua.cl>
2019-01-08 01:59:26 -0300

    add validation detail error on register

--- helpers/validators/validateIdentifier.js
+++ helpers/validators/validateIdentifier.js
@@ -1,11 +1,13 @@
 import get from 'lodash/get'
-
+import { validate } from 'rut.js'
 // Add real RUT validation
 
 export default function validateIdentifier(field, message) {
   message = message ? message : 'El Rut ingresado no es válido'
+
   return form => {
-    const re = /^[0-9]{1,2}[\.][0-9]{3,3}[\.][0-9]{3,3}-[0-9|k]$/
-    if (!re.test(get(form, field, '').toLowerCase())) return message
+    const val = get(form, field, '')
+    const result = validate(val)
+    if (!result) return message
   }
 }
